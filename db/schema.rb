950a32dd6680dc4e7e33cad4105b49f0f4c289a8
ohletamyleta <ohletamyleta@gmail.com>
2021-05-07 15:23:34 -0400

    create and migrate db

---
+++ db/schema.rb
@@ -0,0 +1,54 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_05_07_183619) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "spellbooks", force: :cascade do |t|
+    t.integer "spell_id"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "spells", force: :cascade do |t|
+    t.string "name"
+    t.text "desc"
+    t.text "higher_level"
+    t.string "range"
+    t.string "components"
+    t.string "material"
+    t.string "ritual"
+    t.string "duration"
+    t.string "concentration"
+    t.string "casting_time"
+    t.string "level"
+    t.integer "level_int"
+    t.string "school"
+    t.string "dnd_class"
+    t.string "archetype"
+    t.string "circles"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'rest-client'
+
+resp = RestClient::Request.execute(method: :get,
+  url:'https://api.open5e.com/spells/?limit=400',
+  headers:{
+    'Content-Type': 'application/json'
+  })
+
+  spell_data = JSON.parse(resp.body)
+
+  spells = spell_data["results"]
+
+  #byebug
+
+  
+ spells.each do |spell, attribute|
+    Spell.create(
+      name: spell["name"],
+      desc: spell["desc"],
+      higher_level: spell["higher_level"],
+      range: spell["range"],
+      components: spell["components"],
+      material: spell["material"],
+      ritual: spell["ritual"],
+      duration: spell["duration"],
+      concentration: ["concentration"],
+      casting_time: spell["casting_time"],
+      level: spell["level"],
+      level_int: spell["level_int"],
+      school: spell["school"],
+      dnd_class: spell["dnd_class"],
+      archetype: spell["archetype"],
+      circles: spell["circles"]      
+      )
+  end
+
+User.create(name: "leta", email: "me@me.com", password_digest: "password")
+User.create(name: "sven", email: "why@whynot.com", password_digest: "password")
+
+Spellbook.create(user_id: 2)
+Spellbook.create(user_id: 1)
