4c6788e61997d17f0d2b78e4da07eb465933e029
ohletamyleta <ohletamyleta@gmail.com>
2021-05-07 16:52:43 -0400

    initialize cors & add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,25 @@ Rails.application.routes.draw do
   resources :spellbooks
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post "api/v1/login", to: "api/v1/sessions#create"
+  get "api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
+  delete "api/v1/logout", to: "api/v1/sessions#destroy"
+  post "/api/v1/signup", to: "api/v1/users#create"
+
+  namespace :api do
+    namespace :v1 do
+
+      resources :spells do 
+        resources :users
+      end
+
+      resources :users do
+        resources :spells 
+      end
+
+      resources :spellbooks
+
+    end
+  end
 end
